1b27a3d81ec160402b9f1960ebdacdc55087b2f5
Alena Sharai <exultantislupus@gmail.com>
2024-01-21 23:01:01 +0400

    feat: solve 02-srite-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { stdin, stdout } = require('process');
+
+const output = fs.createWriteStream('./02-write-file/file.txt');
+
+stdout.write('Write some text below: \n');
+stdin.on('data', (data) => {
+  const string = data.toString();
+  const check = string
+    .split('')
+    .filter((el) => el !== '\n')
+    .join('');
+  if (check === 'exit') {
+    stdout.write('Thank you and goodbye!');
+    process.exit();
+  } else {
+    output.write(string);
+  }
+});
+
+process.on('SIGINT', () => {
+  stdout.write('\nThank you and goodbye!');
+  process.exit();
+});
