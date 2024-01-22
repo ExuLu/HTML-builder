b09c6795d39074fd35e629e964d7c1953c2197af
Alena Sharai <exultantislupus@gmail.com>
2024-01-22 10:11:27 +0400

    feat: add error handler to files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,15 +2,24 @@ const path = require('path');
 const fs = require('fs');
 
 fs.readdir('./03-files-in-folder/secret-folder', (err, files) => {
-  files.forEach((file) => {
-    fs.stat(path.join(__dirname, '/secret-folder', file), (err, statistics) => {
-      if (statistics.isFile()) {
-        const info = path.parse(file);
-        let output = `${info.name} - ${info.ext.slice(1)} - ${
-          statistics.size / 1000
-        }kb`;
-        console.log(output);
-      }
+  if (err) console.log(err.message);
+  else {
+    files.forEach((file) => {
+      fs.stat(
+        path.join(__dirname, '/secret-folder', file),
+        (err, statistics) => {
+          if (err) console.log(err.message);
+          else {
+            if (statistics.isFile()) {
+              const info = path.parse(file);
+              let output = `${info.name} - ${info.ext.slice(1)} - ${
+                statistics.size / 1000
+              }kb`;
+              console.log(output);
+            }
+          }
+        },
+      );
     });
-  });
+  }
 });
